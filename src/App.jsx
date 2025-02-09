0a761b5bd97ecf6458192ce87a144a69af427577
i-am-angelo <itsangelof05@gmail.com>
2025-02-09 20:22:50 +0800

    Implement search bar and fetching of movies data

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,95 @@
+import { useState, useEffect } from "react";
+import Search from "./components/Search";
+import Spinner from "./components/Spinner";
+import MovieCard from "./components/MovieCard";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
+
 const App = () => {
+  const [search, setSearch] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage("");
+
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if (!response.ok) {
+        throw new Error("An error occurred while fetching movies");
+      }
+
+      const data = await response.json();
+
+      if (data.Response === "False") {
+        setErrorMessage(
+          data.Error || "An error occurred while fetching movies"
+        );
+        setMovies([]);
+        return;
+      }
+
+      setMovies(data.results || []);
+    } catch (error) {
+      console.error(error);
+      setErrorMessage("An error occurred while fetching movies");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
     <main>
       <div className="pattern" />
 
       <div className="wrapper">
         <header>
+          <img src="./hero.png" alt="Hero Banner" />
+
           <h1>
             Find <span className="text-gradient">Movies</span> You&apos;ll Enjoy
             Without the Hassle
           </h1>
+
+          <Search search={search} setSearch={setSearch} />
         </header>
+
+        <section className="all-movies">
+          <h2 className="mt-[40px]">All Movies</h2>
+
+          {isLoading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movies.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </ul>
+          )}
+
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        </section>
       </div>
     </main>
   );
---
+++ src/components/Search.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+
+const Search = ({ search, setSearch }) => {
+  return (
+    <div className="search">
+      <div>
+        <img src="./search.svg" alt="search" />
+
+        <input
+          type="text"
+          placeholder="Search through thousands of movies"
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+Search.propTypes = {
+  search: PropTypes.string.isRequired,
+  setSearch: PropTypes.func.isRequired,
+};
+
+export default Search;
